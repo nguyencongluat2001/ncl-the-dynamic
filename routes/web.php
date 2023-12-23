6966a48619de1f711bbe04a4c48a65379144946a
luatnc <luatncth2007024@fpt.edu.vn>
2023-12-22 22:02:55 -0800

    f

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/system/login', function () {
-    return view('welcome');
-});
+// Route::get('/system/login', function () {
+//     return view('welcome');
+// });
 
