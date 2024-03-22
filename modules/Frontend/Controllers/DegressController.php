81f0f6cd19db7f7b0dad0d3606da8d3652c649df
HoaiNam <nguyenhoain123@gmail.com>
2024-03-22 09:33:31 +0700

    up

--- modules/Frontend/Controllers/DegressController.php
+++ modules/Frontend/Controllers/DegressController.php
@@ -36,7 +36,7 @@ class DegressController extends Controller
     }
 
     /**
-     * Load màn hình LÀM GIẤY KHÁM SỨC KHỎE
+     * Load màn hình BẰNG CẤP
      * 
      * @return \Illuminate\Contracts\View\View
      */
@@ -48,7 +48,8 @@ class DegressController extends Controller
         $arrResult            = $objLibrary->_getAllFileJavaScriptCssArray('js', 'assets/jquery.validate.js', ',', $arrResult);
         $data['stringJsCss']  = json_encode($arrResult);
         $data['getBlog'] = $this->BlogService->where('status', 1)
-            ->whereIn('code_category', ['TT_02_N1', 'TT_03_N1'])
+            ->where('code_category', 'LIKE', '%TT_02%')
+            ->where('code_category', '!=', 'TT_02')
             ->get();
         if ($data['getBlog']->isNotEmpty()) {
             $blogCodes = $data['getBlog']->pluck('code_blog');
@@ -58,7 +59,6 @@ class DegressController extends Controller
         } else {
             $data['blogs_health'] = collect();
         }
-        // dd($data['blogs_health']);
         return view('Frontend::bangCap.index', $data);
     }
 
